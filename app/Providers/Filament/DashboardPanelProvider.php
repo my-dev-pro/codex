675b410585eb5dc6149fda5c402254dfdf9e855e
Mohamed Youssef <mydev@my-dev.pro>
2024-12-07 09:41:37 +0200

    update Logo and favicon

--- app/Providers/Filament/DashboardPanelProvider.php
+++ app/Providers/Filament/DashboardPanelProvider.php
@@ -29,6 +29,9 @@ class DashboardPanelProvider extends PanelProvider
             ->colors([
                 'primary' => Color::Blue,
             ])
+            ->brandLogo(asset('images/codex-logo.png'))
+            ->brandLogoHeight('3rem')
+            ->favicon(asset('images/codex-favicon.png'))
             ->sidebarCollapsibleOnDesktop()
             ->discoverResources(in: app_path('Filament/Resources'), for: 'App\\Filament\\Resources')
             ->discoverPages(in: app_path('Filament/Pages'), for: 'App\\Filament\\Pages')
