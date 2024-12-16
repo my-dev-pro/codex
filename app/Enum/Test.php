15cb328772ff90c2fee3662c8d330bb8b3be3660
Mohamed Youssef <mydev@my-dev.pro>
2024-12-16 20:43:26 +0200

    Tests list updated

--- app/Enum/Test.php
+++ app/Enum/Test.php
@@ -8,12 +8,13 @@ enum Test : string implements HasLabel
 {
     case CODEX_PRO = 'CodexPro';
     case CODEXI = 'Codexi';
-    case PFIZER_CODEXI = 'Pfizer and Codexi';
     case CODEX_LIQUID = 'Codex Liquid';
     case EXOM = 'Whole Exom Sequencing';
     case MMR = 'MMR';
     case MSI = 'MSI';
+    case PFIZER_CODEXI = 'Pfizer and Codexi';
     case SINGLE_GENE = 'Single Gene';
+    case WES = 'WES';
 
     public function getLabel(): ?string
     {
--- app/Enum/TestFollowUp.php
+++ app/Enum/TestFollowUp.php
@@ -2,10 +2,21 @@
 
 namespace App\Enum;
 
-enum TestFollowUp: string
+use Filament\Support\Contracts\HasLabel;
+
+enum TestFollowUp: string implements HasLabel
 {
-    case NEW = "new";
-    case REFUSED = "refused";
-    case CANCELLED = "cancelled";
-    case COMPLETED = "completed";
+    case NEW = "New";
+    case PICKUP = "Sample pick up";
+    case CENTRAL_HUB = "Sample Received at Central Hub";
+    case CODEX_HUB = "Recieved at Codex Hub";
+    case DELIVERY_AND_REGISTRATION = "Delivery & registeration at PerkinElmer";
+    case REJECTED = "Rejected";
+    case APPROVED = "Approved";
+    case COMPLETED = "Report Released";
+
+    public function getLabel(): ?string
+    {
+        return $this->value;
+    }
 }
