2738e3ddf88c1d8a1919f32c3fa8feef0a8b2861
Mohamed Youssef <mydev@my-dev.pro>
2024-12-18 09:41:54 +0200

    whatsapp config

---
+++ config/whatsapp.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'irsl' => [
+        'server_id' => env('WHATSAPP_IRSL_SERVER_ID', '3'),
+        'instance_id' => env('WHATSAPP_IRSL_INSTANCE_ID', '88'),
+        'token' => env('WHATSAPP_IRSL_TOKEN', 'rtn2LTODN73e790731cb7fec793f3ba6ffe4759f9mVXTQKElF'),
+    ],
+];
